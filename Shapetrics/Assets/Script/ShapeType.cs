942a8053aeed66b947aaea06e40b112c6d001db9
rohithnafeel <devarc16@gmail.com>
2026-07-11 15:05:45 +0530

    Update ShapeType.cs

--- Shapetrics/Assets/Script/ShapeType.cs
+++ Shapetrics/Assets/Script/ShapeType.cs
@@ -1,22 +1,7 @@
-using UnityEngine;
-
-public class ShapeType : MonoBehaviour
+public enum ShapeType
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        public enum ShapeType
-    {
-        Square,
-        Triangle,
-        Circle,
-        Hexagon
-    }
-}
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Square,
+    Triangle,
+    Circle,
+    Hexagon
 }
