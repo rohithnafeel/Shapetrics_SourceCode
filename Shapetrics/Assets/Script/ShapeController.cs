f9948abf04ef4700000fbd7b76158ff0d1781d1f
rohithnafeel <devarc16@gmail.com>
2026-07-11 15:08:53 +0530

    Update ShapeController.cs

--- Shapetrics/Assets/Script/ShapeController.cs
+++ Shapetrics/Assets/Script/ShapeController.cs
@@ -4,18 +4,44 @@ public class ShapeController : MonoBehaviour
 {
     public ShapeType currentShape;
 
-    public ShapeType currentShape;
+    public Sprite square;
+    public Sprite triangle;
+    public Sprite circle;
+    public Sprite hexagon;
 
     private SpriteRenderer sr;
 
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        currentShape = ShapeType.Circle;
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentShape = ShapeType.Square;
+            sr.sprite = square;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            currentShape = ShapeType.Triangle;
+            sr.sprite = triangle;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            currentShape = ShapeType.Circle;
+            sr.sprite = circle;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            currentShape = ShapeType.Hexagon;
+            sr.sprite = hexagon;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
