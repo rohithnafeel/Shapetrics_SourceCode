71d2a93a3a89674cdf96e3890f241e0b3bb6daa9
rohithnafeel <devarc16@gmail.com>
2026-07-12 02:01:17 +0530

    created audio script

---
+++ Shapetrics/Assets/Script/AudioManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioSource sfxSource;
+
+    public AudioClip switchSound;
+    public AudioClip matchSound;
+    public AudioClip gameOverSound;
+    public AudioClip buttonSound;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void PlaySwitch()
+    {
+        sfxSource.PlayOneShot(switchSound);
+    }
+
+    public void PlayMatch()
+    {
+        sfxSource.PlayOneShot(matchSound);
+    }
+
+    public void PlayGameOver()
+    {
+        sfxSource.PlayOneShot(gameOverSound);
+    }
+
+    public void PlayButton()
+    {
+        sfxSource.PlayOneShot(buttonSound);
+    }
+}
