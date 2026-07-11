4dc05f99fe24688e5ade1e6b38bf81ea45a0eb47
rohithnafeel <devarc16@gmail.com>
2026-07-11 15:43:11 +0530

    Update ShapeController.cs

--- Shapetrics/Assets/Script/ShapeController.cs
+++ Shapetrics/Assets/Script/ShapeController.cs
@@ -50,15 +50,20 @@ public class ShapeController : MonoBehaviour
 
         if (obstacle != null)
         {
+            Debug.Log("Player shape: " + currentShape);
+            Debug.Log("Obstacle shape: " + obstacle.obstacleShape);
+
             if (currentShape == obstacle.obstacleShape)
             {
+                Debug.Log("MATCH!");
+
                 GameManager.Instance.AddScore();
 
                 Destroy(other.gameObject);
             }
             else
             {
-                Debug.Log("Game Over!");
+                Debug.Log("NO MATCH!");
 
                 Time.timeScale = 0f;
             }
