cf37fd826aaf614b86b0978bf4773f1388ded58b
rohithnafeel <devarc16@gmail.com>
2026-07-11 11:29:03 +0530

    Updated obstacle script

--- Shapetrics/Assets/Script/Obstacle.cs
+++ Shapetrics/Assets/Script/Obstacle.cs
@@ -11,4 +11,40 @@ public class Obstacle : MonoBehaviour
     }
 
     public Shape obstacleShape;
+
+    public Sprite squareSprite;
+    public Sprite triangleSprite;
+    public Sprite circleSprite;
+    public Sprite hexagonSprite;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetShape(Shape shape)
+    {
+        obstacleShape = shape;
+
+        switch (shape)
+        {
+            case Shape.Square:
+                spriteRenderer.sprite = squareSprite;
+                break;
+
+            case Shape.Triangle:
+                spriteRenderer.sprite = triangleSprite;
+                break;
+
+            case Shape.Circle:
+                spriteRenderer.sprite = circleSprite;
+                break;
+
+            case Shape.Hexagon:
+                spriteRenderer.sprite = hexagonSprite;
+                break;
+        }
+    }
 }
